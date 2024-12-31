018fc29b99488bdef449624b44bb4d2eb270abf1
j3ko <j3ko@users.noreply.github.com>
2024-12-31 12:35:29 +0800

    chore: connect ui

---
+++ app/static/scripts.js
@@ -0,0 +1,26 @@
+const eventSource = new EventSource("/api/status_stream");
+
+eventSource.onmessage = function (event) {
+  const data = JSON.parse(event.data);
+
+  function toggleStatus(selector, isActive) {
+    const elementOn = document.querySelector(`${selector} > .on`);
+    const elementOff = document.querySelector(`${selector} > .off`);
+
+    if (isActive) {
+      elementOn.classList.add("active");
+      elementOff.classList.remove("active");
+    } else {
+      elementOn.classList.remove("active");
+      elementOff.classList.add("active");
+    }
+  }
+
+  toggleStatus(".camera-status", data.is_camera_running);
+  toggleStatus(".recording-status", data.is_recording);
+  toggleStatus(".motion-detection-status", data.is_motion_detecting);
+};
+
+eventSource.onerror = function() {
+  console.error("Error connecting to the status stream.");
+};
