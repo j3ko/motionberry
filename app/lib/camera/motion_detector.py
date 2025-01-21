09f59960bb890cda62f6d3b6b950d34a39e55fce
j3ko <j3ko@users.noreply.github.com>
2025-01-21 16:56:20 +0800

    chore: remove outer loop

--- app/lib/camera/motion_detector.py
+++ app/lib/camera/motion_detector.py
@@ -19,45 +19,33 @@ class MotionDetector:
         prev_frame = None
         w, h = self.camera_manager.detect_size
 
-        while self.is_running:
-            try:
-                self.camera_manager.start_camera()
-                while self.is_running:
-                    try:
-                        cur_frame = self.camera_manager.capture_frame("lores")
-                        cur_frame = cur_frame[:w * h].reshape(h, w)
+        self.camera_manager.start_camera()
+        try:
+            while self.is_running:
+                cur_frame = self.camera_manager.capture_frame("lores")
+                cur_frame = cur_frame[:w * h].reshape(h, w)
 
-                        if prev_frame is not None:
-                            mse = np.square(np.subtract(cur_frame, prev_frame)).mean()
-                            if mse > self.motion_threshold:  # Motion detected
-                                if not self.camera_manager.is_recording:
-                                    self.camera_manager.start_recording()
-                                    self._notify("motion_started")
-                                    self.last_motion_time = time.time()
-                                else:
-                                    self.last_motion_time = time.time()
+                if prev_frame is not None:
+                    mse = np.square(np.subtract(cur_frame, prev_frame)).mean()
+                    if mse > self.motion_threshold:  # Motion detected
+                        if not self.camera_manager.is_recording:
+                            self.camera_manager.start_recording()
+                            self._notify("motion_started")
+                            self.last_motion_time = time.time()
+                        else:
+                            self.last_motion_time = time.time()
 
-                        elif self.camera_manager.is_recording and time.time() - self.last_motion_time > self.max_encoding_time:
-                            final_path = self.camera_manager.stop_recording()
-                            self._notify("motion_stopped", {"filename": str(final_path.name)})
+                    elif self.camera_manager.is_recording and time.time() - self.last_motion_time > self.max_encoding_time:
+                        final_path = self.camera_manager.stop_recording()
+                        self._notify("motion_stopped", {"filename": str(final_path.name)})
 
-                        prev_frame = cur_frame
-                        time.sleep(0.1)
-
-                    except Exception as e:
-                        self.logger.error(f"Error during motion detection iteration: {e}", exc_info=True)
-                        self.logger.info("Attempting to restart motion detection...")
-                        time.sleep(1)
-                        break
-
-            except Exception as e:
-                self.logger.critical(f"Critical error in motion detection loop: {e}", exc_info=True)
-                self.logger.info("Restarting motion detection...")
-                time.sleep(1)
-                continue
-            finally:
-                self.camera_manager.stop_camera()
-                self.logger.info("Motion detection loop terminated and camera stopped.")
+                prev_frame = cur_frame
+                time.sleep(0.1)
+        except Exception as e:
+            self.logger.error(f"Error during motion detection: {e}", exc_info=True)
+        finally:
+            self.camera_manager.stop_camera()
+            self.logger.info("Motion detection loop terminated and camera stopped.")
 
     def start(self):
         """Starts motion detection."""
