40b9a64ad825627cdd27215507add9a7a1b8fc26
j3ko <j3ko@users.noreply.github.com>
2025-01-28 23:46:06 +0800

    chore: try vsync

--- app/lib/camera/video_processor.py
+++ app/lib/camera/video_processor.py
@@ -34,12 +34,12 @@ class VideoProcessor:
                     "-y",
                     "-fflags", "+genpts",
                     "-loglevel", "debug",
+                    "-vsync", "2",
                     "-r", str(self.framerate),
                     # "-framerate", str(self.framerate),
                     "-i", str(h264_path),
                     "-r", str(self.framerate),
                     "-c:v", "copy",
-                    "-video_track_timescale", "30",
                     str(output_path)
                 ],
                 stdout=subprocess.PIPE,
