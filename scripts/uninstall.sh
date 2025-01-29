bafa9d02fb930c7cb7e4d6ada70e056531f01bc7
j3ko <j3ko@users.noreply.github.com>
2025-01-29 11:25:50 +0800

    Revert "chore: use supervisor"
    This reverts commit 87e3c2db888bf9e73ee1f25c40b1430eccdd1f41.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,26 +5,27 @@ set -e
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 APP_DIR=$(realpath "$SCRIPT_DIR/..")
 PYTHON_ENV_DIR="$APP_DIR/.venv"
-SERVICE_NAME="motionberry"
+SERVICE_NAME="motionberry.service"
 
 echo "Uninstalling Motionberry..."
 
-# Stop the Supervisor service
-if supervisorctl status "$SERVICE_NAME" | grep -q 'RUNNING'; then
+if systemctl is-active --quiet "$SERVICE_NAME"; then
     echo "Stopping Motionberry service..."
-    sudo supervisorctl stop "$SERVICE_NAME"
+    sudo systemctl stop "$SERVICE_NAME"
 fi
 
-# Remove Supervisor configuration file
-SUPERVISOR_CONF_FILE="/etc/supervisor/conf.d/$SERVICE_NAME.conf"
-if [ -f "$SUPERVISOR_CONF_FILE" ]; then
-    echo "Removing Supervisor configuration file..."
-    sudo rm -f "$SUPERVISOR_CONF_FILE"
-    sudo supervisorctl reread
-    sudo supervisorctl update
+if systemctl is-enabled --quiet "$SERVICE_NAME"; then
+    echo "Disabling Motionberry service..."
+    sudo systemctl disable "$SERVICE_NAME"
 fi
 
-# # Remove Python virtual environment
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME"
+if [ -f "$SERVICE_FILE" ]; then
+    echo "Removing systemd service file..."
+    sudo rm -f "$SERVICE_FILE"
+    sudo systemctl daemon-reload
+fi
+
 # if [ -d "$PYTHON_ENV_DIR" ]; then
 #     echo "Removing virtual environment..."
 #     rm -rf "$PYTHON_ENV_DIR"
