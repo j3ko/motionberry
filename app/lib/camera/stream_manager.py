f8abe5eff3ce56843accb2811dec760b75e99091
j3ko <j3ko@users.noreply.github.com>
2025-08-16 18:07:43 +0700

    revert: params

--- app/lib/camera/stream_manager.py
+++ app/lib/camera/stream_manager.py
@@ -25,7 +25,6 @@ class StreamManager:
 
         try:
             while True:
-                self.logger.info(f"Capturing frame from camera...{stream}")
                 frame = self.camera_manager.capture_image_array(stream)
 
                 if frame is None:
