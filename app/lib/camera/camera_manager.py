0fd6b021a40d243b2ead49d6da0cc2d3e0af253f
j3ko <j3ko@users.noreply.github.com>
2025-08-17 11:10:01 +0700

    chore: revert

--- app/lib/camera/camera_manager.py
+++ app/lib/camera/camera_manager.py
@@ -93,11 +93,11 @@ class CameraManager:
 
         transform = Transform()
         if self.orientation == 90:
-            transform = Transform(hflip=0, vflip=1)
+            transform = Transform(hflip=1, vflip=0)
         elif self.orientation == 180:
             transform = Transform(hflip=1, vflip=1)
         elif self.orientation == 270:
-            transform = Transform(hflip=1, vflip=0)
+            transform = Transform(hflip=0, vflip=1)
         else:
             transform = Transform()
 
